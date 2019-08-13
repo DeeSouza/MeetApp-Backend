5883fb773426db9fd0247dfde400f0f9f1e51afb
Diego Souza <dee-souza@hotmail.com>
2019-08-13 10:27:59 -0300

    Feat: Send mail to owner meetup with data user enrolled

--- src/app/controllers/EnrolController.js
+++ src/app/controllers/EnrolController.js
@@ -1,6 +1,9 @@
-import { isBefore, parseISO, isEqual } from 'date-fns';
+import { isBefore, parseISO, format, isEqual } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Meetup from '../models/Meetup';
+import User from '../models/User';
 import EnrolMeetup from '../models/EnrolMeetup';
+import Mail from '../../libs/Mail';
 
 class EnrolController {
   /**
@@ -12,7 +15,15 @@ class EnrolController {
   async store(req, res) {
     const { meetup_id } = req.body;
 
-    const meetup = await Meetup.findByPk(meetup_id);
+    const meetup = await Meetup.findByPk(meetup_id, {
+      include: [
+        {
+          model: User,
+          as: 'users',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     // Meetup doesn't exists
     if (!meetup) {
@@ -75,13 +86,34 @@ class EnrolController {
       });
     }
 
+    const enrolled_at = new Date();
     const { id } = await EnrolMeetup.create({
       meetup_id,
+      enrolled_at,
       user_id: req.userId,
-      enrolled_at: new Date(),
     });
 
+    // User logged
+    const userLogged = await User.findByPk(req.userId);
+
     // TODO: Send mail to owner meetup with subscription
+    await Mail.sendMail({
+      to: `${meetup.users.name} <${meetup.users.email}>`,
+      subject: `Inscrição Recebida - ${meetup.title}`,
+      template: 'subscription',
+      context: {
+        year: new Date().getFullYear(),
+        title: meetup.title,
+        owner: meetup.users.name,
+        user: {
+          name: userLogged.name,
+          email: userLogged.email,
+          enrolled_at: format(enrolled_at, "dd 'de' MMMM', ás' H:mm'h'", {
+            locale: pt,
+          }),
+        },
+      },
+    });
 
     return res.json({ id });
   }
