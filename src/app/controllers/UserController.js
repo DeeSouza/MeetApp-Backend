577e6f753dfc4711d4f0ac332658ac92925163c8
Diego Souza <dee-souza@hotmail.com>
2019-09-20 11:20:24 -0300

    Change: Errors message in update user and validation schema yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -51,14 +51,14 @@ class UserController {
    */
   async update(req, res) {
     const schema = Yup.object().shape({
-      name: Yup.string(),
-      email: Yup.string().email(),
-      oldPassword: Yup.string().min(6),
-      password: Yup.string()
-        .min(6)
-        .when('oldPassword', (oldPassword, field) =>
-          oldPassword ? field.required() : field
-        ),
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      oldPassword: Yup.string(),
+      password: Yup.string().when('oldPassword', (oldPassword, field) =>
+        oldPassword ? field.required().min(6) : field
+      ),
       confirmPassword: Yup.string().when('password', (password, field) =>
         password ? field.required().oneOf([Yup.ref('password')]) : field
       ),
@@ -87,7 +87,7 @@ class UserController {
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
       return res
         .status(400)
-        .json({ status: false, error: 'Senhas não conferem.' });
+        .json({ status: false, error: 'Sua senha atual não confere.' });
     }
 
     const { id, name } = await user.update(req.body);
